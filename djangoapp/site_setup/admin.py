2889866ae6f602ac69ca872017b42fcdf6459d2e
Rafael Amaral de Oliveira <rafacienciacomputacao@gmail.com>
2024-05-15 10:12:50 -0300

    Creating the SiteSetup model and registering it in the Django admin

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from site_setup.models import MenuLink
+from django.http import HttpRequest
+from site_setup.models import MenuLink, SiteSetup
 
 
 @admin.register(MenuLink)
@@ -7,3 +8,11 @@ class MenuLinkAdmin(admin.ModelAdmin):
     list_display: tuple = ('id', 'text', 'url_or_path',)
     list_display_links: tuple = ('id', 'text', 'url_or_path',)
     search_fields: tuple = ('id', 'text', 'url_or_path',)
+
+
+@admin.register(SiteSetup)
+class SiteSetupAdmin(admin.ModelAdmin):
+    list_display: tuple = ('title', 'description')
+
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        return not SiteSetup.objects.exists()
