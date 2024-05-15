31421f52ac8213a1c80b5ce6b335120923a28a9b
Rafael Amaral de Oliveira <rafacienciacomputacao@gmail.com>
2024-05-15 16:21:14 -0300

    Relationship of ForeignKey and inlines with admin.TabularInline

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -3,16 +3,22 @@ from django.http import HttpRequest
 from site_setup.models import MenuLink, SiteSetup
 
 
-@admin.register(MenuLink)
-class MenuLinkAdmin(admin.ModelAdmin):
-    list_display: tuple = ('id', 'text', 'url_or_path',)
-    list_display_links: tuple = ('id', 'text', 'url_or_path',)
-    search_fields: tuple = ('id', 'text', 'url_or_path',)
+# @admin.register(MenuLink)
+# class MenuLinkAdmin(admin.ModelAdmin):
+#     list_display: tuple = ('id', 'text', 'url_or_path',)
+#     list_display_links: tuple = ('id', 'text', 'url_or_path',)
+#     search_fields: tuple = ('id', 'text', 'url_or_path',)
+
+
+class MenuLinkInline(admin.TabularInline):
+    model = MenuLink
+    extra = 1
 
 
 @admin.register(SiteSetup)
 class SiteSetupAdmin(admin.ModelAdmin):
     list_display: tuple = ('title', 'description')
+    inlines: tuple = (MenuLinkInline,)
 
     def has_add_permission(self, request: HttpRequest) -> bool:
         return not SiteSetup.objects.exists()
--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -3,21 +3,25 @@ from django.db import models
 
 class MenuLink(models.Model):
     class Meta:
-        verbose_name = 'Menu Link'
-        verbose_name_plural = 'Menu Links'
+        verbose_name: str = 'Menu Link'
+        verbose_name_plural: str = 'Menu Links'
 
     text = models.CharField(max_length=50)
     url_or_path = models.CharField(max_length=2048)
     new_tab = models.BooleanField(default=False)
+    site_setup = models.ForeignKey(
+        'SiteSetup', on_delete=models.CASCADE, blank=True, null=True,
+        default=None,
+    )
 
-    def __str__(self):
-        return self.text
+    def __str__(self) -> str:
+        return str(self.text)
 
 
 class SiteSetup(models.Model):
     class Meta:
-        verbose_name = 'Setup'
-        verbose_name_plural = 'Setup'
+        verbose_name: str = 'Setup'
+        verbose_name_plural: str = 'Setup'
 
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=255)
@@ -29,5 +33,5 @@ class SiteSetup(models.Model):
     show_pagination = models.BooleanField(default=True)
     show_footer = models.BooleanField(default=True)
 
-    def __str__(self):
-        return self.title
+    def __str__(self) -> str:
+        return str(self.title)
