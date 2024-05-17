145545f9b04464b82593c7eab51ed4d3363185ee
Rafael Amaral de Oliveira <rafacienciacomputacao@gmail.com>
2024-05-17 14:50:51 -0300

    Creating Tag and Category + SlugField, slug and slugify models

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from blog.models import Tag, Category
 
-# Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display: tuple = ('id', 'name', 'slug')
+    list_display_links: tuple = ('name',)
+    search_fields: tuple = ('id', 'name', 'slug')
+    list_per_page: int = 10
+    ordering: tuple = ('-id',)
+    prepopulated_fields: dict = {
+        "slug": ('name',),
+    }
+
+
+@admin.register(Category)
+class CagetoryAdmin(admin.ModelAdmin):
+    list_display: tuple = ('id', 'name', 'slug')
+    list_display_links: tuple = ('name',)
+    search_fields: tuple = ('id', 'name', 'slug')
+    list_per_page: int = 10
+    ordering: tuple = ('-id',)
+    prepopulated_fields: dict = {
+        "slug": ('name',),
+    }
